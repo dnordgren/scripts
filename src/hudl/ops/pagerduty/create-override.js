9b02885de699ea887966c3ca1a947b65fb388ab0
Derek Nordgren <derek.nordgren@protonmail.com>
2019-10-20 18:03:50 +0100

    Create Node script for PagerDuty.
    Setting cookies manually in headers doesn't work.
    Hudl's PagerDuty account (or at least my access level) doesn't include
    API access to generate a key.

---
+++ src/hudl/ops/pagerduty/create-override.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+const yargs = require('yargs');
+const fetch = require('node-fetch');
+
+async function main() {
+  const argv = yargs.argv;
+
+  if (argv.helpMe) {
+    help();
+    return;
+  }
+
+  const {
+    schedule,
+    startDate,
+    startTime,
+    endDate,
+    endTime,
+    user,
+    cookie,
+  } = argv;
+
+  if (!schedule || !startDate || !user) {
+    console.error('Invalid input, need to provide at least schedule, startDate and user');
+  }
+
+  if (!cookie) {
+    console.warn('You probably want to provide cookie. Use `document.cookie` from PD > JavaScript Console to get logged-in value');
+  }
+
+  const url = `https://hudl.pagerduty.com/api/v1/schedules/${schedule}/overrides`;
+  const data = {
+    override: {
+      start: `${startDate}T${startTime || '09:30:00'}.000Z`,
+      end: `${endDate || startDate}T${endTime || '17:30:00'}.000Z`,
+      user: {
+        id: user,
+        type: 'user',
+      },
+    },
+  };
+
+  const decodedCookie = Buffer.from(cookie, 'base64').toString('ascii');
+
+  console.log('Request:', JSON.stringify(data));
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      referrer: 'https://hudl.pagerduty.com/schedules',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/vnd.pagerduty+json;version=2',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+        'Cookie': decodedCookie,
+        'DNT': 1,
+        'Pragma': 'no-cache',
+        'X-PagerDuty-Api-Local': '1',
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    });
+
+    const json = await response.json();
+    console.log('Override created:', json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function help() {
+  console.log(
+    ' _____                      _____        _         _       _    _ _____                  _         \n',
+    '|  __ \                    |  __ \      | |       ( )     | |  | |_   _|                | |        \n',
+    '| |__) |_ _  __ _  ___ _ __| |  | |_   _| |_ _   _|/ ___  | |  | | | |    ___ _   _  ___| | _____  \n',
+    '|  ___/ _` |/ _` |/ _ \ ,__| |  | | | | | __| | | | / __| | |  | | | |   / __| | | |/ __| |/ / __| \n',
+    '| |  | (_| | (_| |  __/ |  | |__| | |_| | |_| |_| | \__ \ | |__| |_| |_  \__ \ |_| | (__|   <\__\  \n',
+    '|_|   \__ _|\__  |\___|_|  |_____/ \__ _|\__|\__  | |___/  \____/|_____| |___/\__ _|\___|_|\_\___/ \n',
+    '             __/ |                            __/ |                                                \n',
+    '            |___/                            |___/                                                   '
+  );
+
+  console.log('Arguments:', [
+    'schedule',
+    'user',
+    'cookie (base64 encoded e.g. `Buffer.from("<cookie>").toString("base64")` in Node or `btoa("<cookie>")` in JavaScript Console)',
+    'startDate',
+    'startTime (optional, defaults to 09:30, relative to your user timezone preference)',
+    'endDate (optional, defaults to $startDate)',
+    'endTime (optional, defaults to 17:30, relative to your user timezone preference)',
+  ]);
+
+  console.log({
+    'Connect High Urgency': 'PN59INH',
+    'Connect Low Urgency': 'P77U0AW',
+    'First Tier': 'PZY553V',
+  });
+}
+
+main();
