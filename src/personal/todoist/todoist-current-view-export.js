7ce1b590331711d97bb4e945ad27497d0c0f306d
Derek Nordgren <derek.nordgren@protonmail.com>
2019-10-20 20:54:23 +0100

    Add various clipboard support methods.

--- src/personal/todoist/todoist-current-view-export.js
+++ src/personal/todoist/todoist-current-view-export.js
@@ -1,8 +1,28 @@
 // Run from JavaScript Console.
-const tasks = document.getElementsByClassName('task_item_content_text');
-const output = [];
-for (let task of tasks) {
-  output.push(task.textContent);
+function main() {
+  const tasksText = Array
+    .from(
+      document.getElementsByClassName('task_item_content_text'),
+      task => (task.textContent))
+    .join('\n');
+
+  // Need permission to write directly to clipboard (?)
+  // const success = await navigator.clipboard.writeText(tasksText);
+
+  window.alert(tasksText);
+
+  // Select alert text, copy to clipboard.
+  // Won't work in Firefox, may work in Chrome.
+  const range = document.createRange();
+  range.selectNode(document.querySelector('.text_holder'));
+
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+
+  // Copy via console.
+  copy(tasksText);
 }
 
-alert(output.join("\n"));
+main();
